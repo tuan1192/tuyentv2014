e0e4f9f1e819cd6654b6111bc4deaa0f4ab724b4
Nguyễn Đức Tuấn <tuan.m4u@gmail.com>
2014-09-07 00:41:54 +0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function ($) {
+
+
+    $(window).stellar();
+
+    var links = $('.navigation').find('li');
+    slide = $('.slide');
+    button = $('.button');
+    mywindow = $(window);
+    htmlbody = $('html,body');
+
+    function goToByScroll(dataslide) {
+        htmlbody.animate({
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top - 85
+        }, 1000, 'easeInOutQuint');
+    }
+
+    links.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+
+    button.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+
+    });
+
+
+});
